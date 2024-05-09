10acdf7695acd5bfa202efb02c379a2f7d8a0b36
void2610 <izumisan2610@gmail.com>
2024-05-10 00:27:35 +0900

    dash particle

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour
     private float speed = 10.0f;
     private Rigidbody rb;
     private bool canDash = true;
+    private ParticleSystem dashParticle;
     private Vector3 GetMoveDirection()
     {
         Vector3 moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
@@ -29,6 +30,10 @@ public class Player : MonoBehaviour
     {
         rb = this.GetComponent<Rigidbody>();
         rb.velocity = Vector3.zero;
+        GameObject dash = Instantiate(Resources.Load<GameObject>("Prefabs/Particle/DashParticle"), this.transform.position + new Vector3(0, 0, -0.5f), Quaternion.identity);
+        dash.transform.SetParent(this.transform);
+        dashParticle = dash.GetComponent<ParticleSystem>();
+        dashParticle.Stop();
     }
 
     // Update is called once per frame
@@ -40,6 +45,7 @@ public class Player : MonoBehaviour
         {
             rb.AddForce(this.transform.forward * 12 * StatusManager.instance.DASH_POWER, ForceMode.Impulse);
             StartCoroutine("DashColldown");
+            dashParticle.Play();
         }
     }
 
